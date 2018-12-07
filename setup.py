5533d855144535d98fc5324fb4fec4e51e2a355e
Moritz Lürig <moritz.luerig@gmail.com>
2018-12-07 16:48:11 +0100

    rm ocr/morpho

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Moritz Lürig',
     author_email='moritz.luerig@eawag.ch',
     packages=['phenopype'],
-    install_requires=["pandas", "opencv-contrib-python>=3.4.4", "pytesseract", "trackpy", "exifread"],
+    install_requires=["pandas", "opencv-contrib-python>=3.4.4", "exifread"], #"pytesseract", "trackpy",
     version='0.4.7',
     license='LGPL',
     description='a phenotyping pipeline for python',
