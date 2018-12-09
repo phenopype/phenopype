2cd8b925d618fa037119e2fab146bea1d18d1397
Moritz Lürig <moritz.luerig@gmail.com>
2018-12-09 14:41:34 +0100

    example update

--- phenopype/__init__.py
+++ phenopype/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Last Update: 2018/10/07
-Version 0.4.0
-@author: Moritz Lürig
-"""
 
 __all__ = []
 
@@ -13,8 +8,6 @@ import inspect
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-#__path__="E:\\Python1\\phenopype\\"
 
 for loader, name, is_pkg in pkgutil.walk_packages(__path__):
     module = loader.find_module(name).load_module(name)
@@ -26,6 +19,3 @@ for loader, name, is_pkg in pkgutil.walk_packages(__path__):
         globals()[name] = value
         __all__.append(name)
         
-#from phenopype import utils, base, custom
-#pp.__all__ + ["utils", "base", "custom"]
-
