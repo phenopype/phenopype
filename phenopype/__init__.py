8d23bb44493ea2f2a3961b5c3f6770eb7ef90b72
Moritz Luerig <moritz.luerig@gmail.com>
2018-12-09 17:55:31 +0100

    pre versioneer

--- phenopype/__init__.py
+++ phenopype/__init__.py
@@ -5,14 +5,6 @@ __all__ = []
 import pkgutil
 import inspect
 
-<<<<<<< HEAD
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-=======
-#__path__="E:\\Python1\\phenopype\\"
->>>>>>> parent of 1439bed... versioneer added
-
 for loader, name, is_pkg in pkgutil.walk_packages(__path__):
     module = loader.find_module(name).load_module(name)
 
@@ -22,10 +14,5 @@ for loader, name, is_pkg in pkgutil.walk_packages(__path__):
 
         globals()[name] = value
         __all__.append(name)
-<<<<<<< HEAD
-        
-=======
-        
-#from phenopype import utils, base, custom
-#pp.__all__ + ["utils", "base", "custom"]
->>>>>>> parent of 1439bed... versioneer added
+
+from _version import __version__
--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup
 
+import re
+VERSIONFILE="myniftyapp/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+    
+    
 setup(
     name='phenopype',
     url='https://github.com/mluerig/phenopype',
@@ -7,7 +18,7 @@ setup(
     author_email='moritz.luerig@eawag.ch',
     packages=['phenopype'],
     install_requires=['setuptools-git-version', "pandas", "opencv-contrib-python>=3.4.4", "exifread", "Pillow", "pytesseract", "trackpy"], 
-    version='{tag}.dev{commitcount}+{gitsha}',
+    version=verstr,
     license='LGPL',
     description='a phenotyping pipeline for python',
     long_description=open('README.md').read(),
