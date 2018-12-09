a9ed3a2edb053dca5b909f4d22a97e4991176458
Moritz Lürig <moritz.luerig@gmail.com>
2018-12-09 14:13:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import versioneer
 
 setup(
     name='phenopype',
@@ -7,8 +8,11 @@ setup(
     author_email='moritz.luerig@eawag.ch',
     packages=['phenopype'],
     install_requires=["pandas", "opencv-contrib-python>=3.4.4", "exifread"], #"pytesseract", "trackpy",
-    version='0.4.7',
+    version=versioneer.get_version(),
     license='LGPL',
     description='a phenotyping pipeline for python',
     long_description=open('README.md').read(),
 )
+
+  version=versioneer.get_version(),
+  cmdclass=versioneer.get_cmdclass(),
