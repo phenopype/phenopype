def481511fa50963a3abfc66e5b3671e30e0ae11
Moritz Luerig <moritz.luerig@gmail.com>
2019-01-26 19:53:26 +0100

    docs

--- setup.py
+++ setup.py
@@ -12,16 +12,17 @@ else:
     
     
 setup(
-    name='phenopype',
-    url='https://github.com/mluerig/phenopype',
-    author='Moritz Luerig',
-    author_email='moritz.luerig@eawag.ch',
-    packages=['phenopype'],
-    install_requires=["pandas", "opencv-contrib-python>=3.4.4", "exifread"], 
-    version=verstr,
-    license='LGPL',
-    description='a phenotyping pipeline for python',
-    long_description=open('README.md').read(),
+	name='phenopype',
+	url='https://github.com/mluerig/phenopype',
+	author='Moritz Luerig',
+	author_email='moritz.luerig@eawag.ch',
+	packages=['phenopype'],
+	install_requires=["pandas", "opencv-contrib-python==3.4.4", "exifread"], 
+	version=verstr,
+	license='LGPL',
+	description='a phenotyping pipeline for python',
+	long_description=open('README.md').read(), 
+	long_description_content_type='text/markdown'
 )
 
 # to come
