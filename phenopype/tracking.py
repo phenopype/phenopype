21eefa38d85001b2920e2976132b2a1a7a946f40
Moritz Luerig <moritz.luerig@gmail.com>
2019-02-04 16:02:33 +0100

    basic fgbg working

--- phenopype/tracking.py
+++ phenopype/tracking.py
@@ -3,7 +3,6 @@ from __future__ import division, unicode_literals, print_function  # for compati
 import numpy as np
 import cv2
 import math
-import copy
 import os
 
 from phenopype.utils import (blur)
@@ -12,6 +11,141 @@ from phenopype.utils import (blur)
 def avgit(x):
     return x.sum(axis=0)/np.shape(x)[0]
 
+
+def decode_fourcc(cc):
+    return "".join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
+              
+       
+class video(object):
+    def get_properties(self, video_path, **kwargs):    
+        
+        self.video_path = video_path       
+        self.name = os.path.basename(self.video_path)
+
+
+        if isinstance(self.video_path, str):
+            cap = cv2.VideoCapture(self.video_path)
+            ret, frame = cap.read()
+        else:
+            print("Error - str path to video needed")
+            
+        self.nframes = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
+        self.fps = cap.get(cv2.CAP_PROP_FPS)
+        self.fourcc_str = decode_fourcc(cap.get(cv2.CAP_PROP_FOURCC))
+        self.length = str(int(( self.nframes / self.fps)/60)).zfill(2) + ":" +str(int((((self.nframes / self.fps)/60)-int((self.nframes / self.fps)/60))*60)).zfill(2)
+        self.dimensions = frame.shape[0:2]
+        if frame.shape[2] == 3:
+            self.is_colour_video = True
+        
+        print("\n")
+        print("----------------------------------------------------------------")
+        print("Video properties of \"" + self.name + "\":\n")
+        print("Frames per second: " + str(self.fps) + "\nN frames: " + str(self.nframes) + "\nLength: " + str(self.length) + " (mm:ss)" + "\nDimensions: " + str(self.dimensions) + "\nColour video: " + str(self.is_colour_video) + "\nFourCC code: " + str(self.fourcc_str)) 
+        print("----------------------------------------------------------------")
+
+
+    def tracking_setup(self, **kwargs):
+        
+        name_nosuf = os.path.splitext(os.path.basename(self.video_path))[0]
+        suffix = kwargs.get("suffix", "out")
+        name_out = kwargs.get("name", name_nosuf + "_" + suffix + ".avi")  
+        dir_out = kwargs.get("save_dir", os.path.dirname(self.video_path))
+        if not os.path.exists(dir_out):
+            os.makedirs(dir_out)
+        self.path_out = os.path.join(dir_out, name_out)
+            
+        fps_out = kwargs.get("fps", self.fps)
+        dim_out = kwargs.get("dimensions", tuple(reversed(self.dimensions)))
+        self.fourcc_str_out = kwargs.get("fourcc_string", "DIVX")
+        self.fourcc_out = cv2.VideoWriter_fourcc(*self.fourcc_str_out)
+        self.save_as_colour = kwargs.get("save_as_colour", self.is_colour_video)
+
+        history = kwargs.get("history", 60)
+        backgr_thresh = kwargs.get("backgr_thresh", 10)        
+        detect_shadows = kwargs.get("detect_shadows", True)
+        
+        self.skip = kwargs.get("skip", 5)
+        self.wait = kwargs.get("start_after", 15)
+        if "finish_after" in kwargs:
+            self.finish = kwargs.get("finish_after")
+
+        self.writer = cv2.VideoWriter(self.path_out, self.fourcc_out, fps_out, dim_out, self.save_as_colour) 
+        self.fgbg_subtractor = cv2.createBackgroundSubtractorMOG2(history = int(history * (self.fps / self.skip)), varThreshold = int(backgr_thresh), detectShadows = detect_shadows)
+
+        print("\n")
+        print("----------------------------------------------------------------")
+        print("Settings for motion tracking in \"" + self.name + "\":\n")
+        print("Save name: " + name_out + "\nSave path: " + os.path.dirname(self.video_path) + "\nFrames per second: " + str(fps_out) + "\nDimensions: " + str(dim_out) + "\nFormat (FourCC code) : " + self.fourcc_str_out + "\n\"History\"-parameter: " + str(history) + " seconds" + "\nSensitivity: " + str(backgr_thresh) + "\nRead every nth frame: " + str(self.skip) + "\nDetect shadows: " + str(detect_shadows) + "\nStart after n seconds: " + str(self.wait) + "\nFinish after n seconds: " + str(self.finish if self.finish else " - ")) 
+        print("----------------------------------------------------------------")
+        
+    def tracking(self, **kwargs):
+        
+        self.idx1, self.idx2 = (0,0)
+        self.capture = cv2.VideoCapture(self.video_path)        
+        self.wait_frame = self.wait * self.fps
+        if self.finish:
+            self.finish_frame = self.finish * self.fps
+
+        while(self.capture.isOpened()):
+            
+            # read video 
+            ret, self.frame = self.capture.read()     
+            self.capture_frame = False
+            if ret==False:
+                break
+            
+            # indexing 
+            self.idx1, self.idx2 = (self.idx1 + 1,  self.idx2 + 1)    
+            if self.idx2 == self.skip:
+                self.idx2 = 0    
+            if self.finish_frame:
+                if self.idx1 >= self.finish_frame:
+                    break
+                             
+                
+            # warmup fgbg-algorithm shortly before capturing 
+            if self.idx1 > self.wait_frame - (3*self.fps) and self.idx2 == 0:
+                self.fgmask = self.fgbg_subtractor.apply(self.frame)               
+                
+            # start modules after x seconds
+            if self.idx1 > self.wait_frame and self.idx2 == 0:
+                self.capture_frame = True
+                
+            
+            # feedback
+            self.mins = str(int((self.idx1 / self.fps)/60)).zfill(2)
+            self.secs = str(int((((self.idx1 / self.fps)/60)-int(self.mins))*60)).zfill(2)    
+            self.time_stamp = "Time: " + self.mins + ":" + self.secs + "/" + self.length + " - Frames: " + str(self.idx1) + "/" + str(int(self.nframes))
+            if self.capture_frame == True:
+                print(self.time_stamp + " - captured")
+            else:
+                print(self.time_stamp)
+                
+                
+            # video writer  
+            if self.capture_frame == True:    
+
+                cv2.namedWindow('phenopype' ,cv2.WINDOW_NORMAL)
+                cv2.imshow('phenopype', self.fgmask)    
+                self.writer.write(self.fgmask)
+            
+            # keep stream open
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        # cleanup
+        self.capture.release()
+        self.writer.release()
+        cv2.destroyAllWindows()
+        
+        
+#class test_module(object):
+#    def track(**kwargs):
+#        fgmask = video.fgmask
+#        if "blur" in kwargs:
+#            blur_kernel, thresh_val = kwargs.get("blur",[9, 127])
+#            blurred = blur(fgmask, blur_kernel)
+
+ 
 
 class motion_tracker(object):
     def __init__(self, frame, fg_mask, min_length, **kwargs):
@@ -67,22 +201,15 @@ class motion_tracker(object):
         else: 
             self.empty = True
             
-    def draw(self, frame, arguments):
-        frame_out = frame
-        
-        if not self.empty:
-            if "contour" in arguments:
-                frame_out = cv2.addWeighted(frame_out, 1, self.contour_drawn, 0.5, 0)
-            if "ellipse" in arguments:
-                frame_out = cv2.ellipse(frame_out, tuple(self.ellipse[0:3]), (0,0,255), 3)
-            if "text" in arguments:
-                frame_out = cv2.putText(frame_out, "Fish", (int(self.center[0,0]), int(self.center[0,1])), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA)  
-            return frame_out
-        
-        else:
-            return frame_out
 
-                 
+        frame_out = frame
+        frame_out = cv2.addWeighted(frame_out, 1, self.contour_drawn, 0.5, 0)
+        frame_out = cv2.ellipse(frame_out, tuple(self.ellipse[0:3]), (0,0,255), 3)
+        frame_out = cv2.putText(frame_out, "Fish", (int(self.center[0,0]), int(self.center[0,1])), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA)  
+
+
+        
+
 class isopod_module(object):
     def __init__(self, frame, fgmask, shadows, blur_kern, blur_thresh, min_length, max_length, arena_mask):
         
@@ -141,153 +268,3 @@ class isopod_module(object):
         
         else:
             return frame_out          
-       
-class video(object):
-    def get_info(self, video_path):    
-        
-        self.video_path = video_path
-        if isinstance(self.video_path, str):
-            cap = cv2.VideoCapture(self.video_path)
-        else:
-            print("Error - str path to video needed")
-            
-        self.nframes = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
-        self.fps = cap.get(cv2.CAP_PROP_FPS)
-        self.length = str(int(( self.nframes / self.fps)/60)).zfill(2) + ":" +str(int((((self.nframes / self.fps)/60)-int((self.nframes / self.fps)/60))*60)).zfill(2)
-
-        idx = 0
-        while(cap.isOpened()):
-            idx = idx + 1
-            # extract frame from video stream
-            ret, frame = cap.read()
-            if idx == 5:
-                break
-            
-        self.name = os.path.splitext(os.path.basename(self.video_path))[0]
-        self.frame = frame
-        self.height = frame.shape[0]
-        self.width = frame.shape[1]
-        self.layers = frame.shape[2]
-
-    def io(self, video_format="DIVX", backgr_thresh=10, **kwargs):  
-        
-        shadows = kwargs.get("detect_shadows", True)
-        our_dir = kwargs.get("save_dir", os.path.dirname(self.video_path))
-        suffix = kwargs.get("suffix", "out")
-        
-        history = kwargs.get("history", 1)
-        backgr_thresh = kwargs.get("backgr_thresh", 10)        
-        skip = kwargs.get("skip", 4)
-
-        fourcc = cv2.VideoWriter_fourcc(*video_format)
-        self.writer = cv2.VideoWriter(os.path.join(our_dir,  self.name + "_" + suffix + ".avi"), fourcc, self.fps, (self.width, self.height), True) 
-        self.fgbg_subtractor = cv2.createBackgroundSubtractorMOG2(history = int((history*60) * (self.fps / skip)), varThreshold = int(backgr_thresh), detectShadows = shadows)
-
-    def tracking(self):
-        idx1, idx2 = (0,0)
-        df_fish, df_isopod  = (pd.DataFrame(),pd.DataFrame())
-        cap = cv2.VideoCapture(video_path)
-
-            
-    
-        
-class video_time(object):
-    def __init__(self, idx, fps):
-        self.mins = str(int((idx / fps)/60)).zfill(2)
-        self.secs = str(int((((idx / fps)/60)-int(self.mins))*60)).zfill(2)
-        
-def capture_feedback(mins, secs, index, length, nframes, capture):       
-    if capture == True:
-        print(mins + ":" + secs + " / " + length + " - " + str(index) + " / " + str(int(nframes)) + " - captured")
-    else:
-        print(mins + ":" + secs + " / " + length + " - " + str(index) + " / " + str(int(nframes)))
-
-class polygon_drawer(object):
-    
-    # initiate
-    def __init__(self, video_name, save_path):
-        self.window_name = video_name # Name for our window
-        self.video_name = video_name
-        self.save_path = save_path # Name for our window
-        self.done = False # Flag signalling we're done
-        self.current = (0, 0) # Current position, so we can draw the line-in-progress
-        self.points = [] # List of points defining our polygon
-        
-        self.FINAL_LINE_COLOR = (0, 255, 0)
-        self.FILL_COLOR = (255,255,255)
-        self.WORKING_LINE_COLOR = (255, 0, 0)
-
-    # mouse action
-    def on_mouse(self, event, x, y, buttons, user_param):
-        # Mouse callback that gets called for every mouse event (i.e. moving, clicking, etc.)
-
-        if self.done: # Nothing more to do
-            return
-
-        if event == cv2.EVENT_MOUSEMOVE:
-            # We want to be able to draw the line-in-progress, so update current mouse position
-            self.current = (x, y)
-        if event == cv2.EVENT_LBUTTONDOWN:
-            # Left click means adding a point at current position to the list of points
-            print("Adding point #%d with position(%d,%d)" % (len(self.points), x, y))
-            self.points.append((x, y))
-        elif event == cv2.EVENT_RBUTTONDOWN:
-            # Right click means we're done
-            print("Completing polygon with %d points." % len(self.points))
-            self.done = True
-
-    # draw lines
-    def run(self, image):
-        # Let's create our working window and set a mouse callback to handle events
-        cv2.namedWindow(self.window_name, flags=cv2.WINDOW_NORMAL)
-        cv2.imshow(self.window_name, image)
-        cv2.waitKey(1)
-        cv2.setMouseCallback(self.window_name, self.on_mouse)
-        
-        temp_canvas = copy.deepcopy(image)
-
-        while(not self.done):
-            # This is our drawing loop, we just continuously draw new images
-            # and show them in the named window
-            if (len(self.points) > 0):
-                # Draw all the current polygon segments
-                cv2.polylines(temp_canvas, np.array([self.points]), False, self.FINAL_LINE_COLOR, 1)
-                # And  also show what the current segment would look like
-                cv2.line(temp_canvas, self.points[-1], self.current, self.WORKING_LINE_COLOR)
-            # Update the window
-            cv2.imshow(self.window_name, temp_canvas)
-            temp_canvas = copy.deepcopy(image)
-            # And wait 50ms before next iteration (this will pump window messages meanwhile)
-            if cv2.waitKey(50) == 27: # ESC hit
-                self.done = True
-
-        # create final arena
-        canvas = image
-        zeros = np.zeros(canvas.shape, np.uint8)
-        red = np.zeros(canvas.shape, np.uint8)
-        red[:,:,2] = 255
-        
-        if (len(self.points) > 0):
-            cv2.fillPoly(red, np.array([self.points]), self.FINAL_LINE_COLOR)
-               
-        if (len(self.points) > 0):
-            cv2.fillPoly(zeros, np.array([self.points]), self.FILL_COLOR)
-            
-        # return for further use
-        self.mask_colour = zeros
-        self.mask_gray = cv2.cvtColor(zeros, cv2.COLOR_BGR2GRAY)
-        self.image = cv2.addWeighted(copy.deepcopy(canvas), .8, red, 0.2, 0)
-        self.points = np.array([self.points])
-        self.rect = cv2.boundingRect(self.points)
-
-        # Waiting for the user to press any key and return points
-        cv2.imshow(self.window_name, self.image)
-        save_image_path = os.path.join(self.save_path, self.video_name + "_arena.png")
-        cv2.imwrite(save_image_path, self.image)    
-        
-        # show image of polygon
-        cv2.waitKey()
-        cv2.destroyWindow(self.window_name)
-        print("Arena image saved: " + save_image_path)
-
-  
