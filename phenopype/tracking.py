b32500d9cb3d1ba1d645b51f42a882130f041db4
Moritz Luerig <moritz.luerig@gmail.com>
2019-02-05 22:54:02 +0100

    tracking module framework done

--- phenopype/tracking.py
+++ phenopype/tracking.py
@@ -4,8 +4,10 @@ import numpy as np
 import cv2
 import math
 import os
+import pprint
 
 from phenopype.utils import (blur)
+
 
 
 def avgit(x):
@@ -16,76 +18,103 @@ def decode_fourcc(cc):
     return "".join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
               
        
-class video(object):
-    def get_properties(self, video_path, **kwargs):    
-        
-        self.video_path = video_path       
-        self.name = os.path.basename(self.video_path)
-
-
-        if isinstance(self.video_path, str):
-            cap = cv2.VideoCapture(self.video_path)
+class motion_tracker(object):
+    def __init__(self, video_path, **kwargs): 
+        """Read properties of input video file.
+        """
+        if isinstance(video_path, str):
+            cap = cv2.VideoCapture(video_path)
             ret, frame = cap.read()
-        else:
-            print("Error - str path to video needed")
             
+
+        else:
+            print("Error - full or relative path of video-file needed")
+        
+        self.path = video_path       
+        self.name = os.path.basename(self.path)
         self.nframes = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
         self.fps = cap.get(cv2.CAP_PROP_FPS)
         self.fourcc_str = decode_fourcc(cap.get(cv2.CAP_PROP_FOURCC))
         self.length = str(int(( self.nframes / self.fps)/60)).zfill(2) + ":" +str(int((((self.nframes / self.fps)/60)-int((self.nframes / self.fps)/60))*60)).zfill(2)
-        self.dimensions = frame.shape[0:2]
+        self.dimensions = tuple(reversed(frame.shape[0:2]))
         if frame.shape[2] == 3:
-            self.is_colour_video = True
+            self.is_colour = True
+            
+        cap.release()
         
         print("\n")
         print("----------------------------------------------------------------")
-        print("Video properties of \"" + self.name + "\":\n")
-        print("Frames per second: " + str(self.fps) + "\nN frames: " + str(self.nframes) + "\nLength: " + str(self.length) + " (mm:ss)" + "\nDimensions: " + str(self.dimensions) + "\nColour video: " + str(self.is_colour_video) + "\nFourCC code: " + str(self.fourcc_str)) 
+        print("Input video properties - \"" + self.name + "\":\n")
+        print("Frames per second: " + str(self.fps) + "\nN frames: " + str(self.nframes) + "\nLength: " + str(self.length) + " (mm:ss)" + "\nDimensions: " + str(self.dimensions) + "\nColour video: " + str(self.is_colour) + "\nFourCC code: " + str(self.fourcc_str)) 
         print("----------------------------------------------------------------")
 
 
-    def tracking_setup(self, **kwargs):
-        
-        name_nosuf = os.path.splitext(os.path.basename(self.video_path))[0]
-        suffix = kwargs.get("suffix", "out")
-        name_out = kwargs.get("name", name_nosuf + "_" + suffix + ".avi")  
-        dir_out = kwargs.get("save_dir", os.path.dirname(self.video_path))
+    def video_output(self, **kwargs):
+        """Set properties of output video file .
+        """
+        name_out = kwargs.get("name",  os.path.splitext(self.name)[0] + "_" + kwargs.get("suffix", "out") + ".avi")  
+        dir_out = kwargs.get("save_dir", os.path.dirname(self.path))
         if not os.path.exists(dir_out):
             os.makedirs(dir_out)
-        self.path_out = os.path.join(dir_out, name_out)
+        path_out = os.path.join(dir_out, name_out)
             
         fps_out = kwargs.get("fps", self.fps)
-        dim_out = kwargs.get("dimensions", tuple(reversed(self.dimensions)))
-        self.fourcc_str_out = kwargs.get("fourcc_string", "DIVX")
-        self.fourcc_out = cv2.VideoWriter_fourcc(*self.fourcc_str_out)
-        self.save_as_colour = kwargs.get("save_as_colour", self.is_colour_video)
-
-        history = kwargs.get("history", 60)
-        backgr_thresh = kwargs.get("backgr_thresh", 10)        
-        detect_shadows = kwargs.get("detect_shadows", True)
+        dimensions_out = kwargs.get("dimensions", self.dimensions)
+        self.resize_factor = kwargs.get("resize", 1)
+        dimensions_out = (int(dimensions_out[0] * self.resize_factor), int(dimensions_out[1] * self.resize_factor))
+        if self.resize_factor < 1:
+            res_msg = " (original resized by factor " + str(self.resize_factor) + ")"
+        else:
+            res_msg = ""
+        fourcc_str_out = kwargs.get("fourcc_string", "DIVX")
+        fourcc_out = cv2.VideoWriter_fourcc(*fourcc_str_out)
+        save_colour = kwargs.get("save_colour", self.is_colour)
         
+        self.writer = cv2.VideoWriter(path_out, fourcc_out, fps_out, dimensions_out, save_colour) 
+
+
+        print("\n")
+        print("----------------------------------------------------------------")
+        print("Output video settings - \"" + self.name + "\":\n")
+        print("Save name: " + name_out + "\nSave dir: " + dir_out + "\nFrames per second: " + str(fps_out) + "\nDimensions: " + str(dimensions_out) + res_msg +  "\nColour video: " + str(save_colour) + "\nFormat (FourCC code): " + fourcc_str_out) 
+        print("----------------------------------------------------------------")
+        
+    def motion_detection(self, **kwargs):
+
         self.skip = kwargs.get("skip", 5)
         self.wait = kwargs.get("start_after", 15)
         if "finish_after" in kwargs:
             self.finish = kwargs.get("finish_after")
+        history = kwargs.get("history", 60)
+        backgr_thresh = kwargs.get("backgr_thresh", 10)        
+        self.detect_shadows = kwargs.get("detect_shadows", True)
 
-        self.writer = cv2.VideoWriter(self.path_out, self.fourcc_out, fps_out, dim_out, self.save_as_colour) 
-        self.fgbg_subtractor = cv2.createBackgroundSubtractorMOG2(history = int(history * (self.fps / self.skip)), varThreshold = int(backgr_thresh), detectShadows = detect_shadows)
-
+        self.fgbg_subtractor = cv2.createBackgroundSubtractorMOG2(history = int(history * (self.fps / self.skip)), varThreshold = int(backgr_thresh), detectShadows = self.detect_shadows)
+        
+        if "methods" in kwargs:
+            self.methods = kwargs.get("methods")
+            for m in self.methods:
+                m.print_settings()
+        
         print("\n")
         print("----------------------------------------------------------------")
-        print("Settings for motion tracking in \"" + self.name + "\":\n")
-        print("Save name: " + name_out + "\nSave path: " + os.path.dirname(self.video_path) + "\nFrames per second: " + str(fps_out) + "\nDimensions: " + str(dim_out) + "\nFormat (FourCC code) : " + self.fourcc_str_out + "\n\"History\"-parameter: " + str(history) + " seconds" + "\nSensitivity: " + str(backgr_thresh) + "\nRead every nth frame: " + str(self.skip) + "\nDetect shadows: " + str(detect_shadows) + "\nStart after n seconds: " + str(self.wait) + "\nFinish after n seconds: " + str(self.finish if self.finish else " - ")) 
+        print("Motion detection settings - \"" + self.name + "\":\n")
+        print("\n\"History\"-parameter: " + str(history) + " seconds" + "\nSensitivity: " + str(backgr_thresh) + "\nRead every nth frame: " + str(self.skip) + "\nDetect shadows: " + str(self.detect_shadows) + "\nStart after n seconds: " + str(self.wait) + "\nFinish after n seconds: " + str(self.finish if self.finish else " - ")) 
         print("----------------------------------------------------------------")
+              
+         
         
-    def tracking(self, **kwargs):
         
+    def run_tracking(self, **kwargs):
+                
         self.idx1, self.idx2 = (0,0)
-        self.capture = cv2.VideoCapture(self.video_path)        
+        self.capture = cv2.VideoCapture(self.path)        
         self.wait_frame = self.wait * self.fps
         if self.finish:
             self.finish_frame = self.finish * self.fps
+            
 
+        #methods_out = []
         while(self.capture.isOpened()):
             
             # read video 
@@ -110,60 +139,80 @@ class video(object):
             # start modules after x seconds
             if self.idx1 > self.wait_frame and self.idx2 == 0:
                 self.capture_frame = True
-                
-            
+                           
             # feedback
             self.mins = str(int((self.idx1 / self.fps)/60)).zfill(2)
             self.secs = str(int((((self.idx1 / self.fps)/60)-int(self.mins))*60)).zfill(2)    
             self.time_stamp = "Time: " + self.mins + ":" + self.secs + "/" + self.length + " - Frames: " + str(self.idx1) + "/" + str(int(self.nframes))
-            if self.capture_frame == True:
+
+
+                
+            if self.capture_frame == True:    
+                for m in self.methods:
+                    self.show = m._run(self.frame, self.fgmask, self.resize_factor)
                 print(self.time_stamp + " - captured")
+                
+                self.writer.write(self.show)
+                cv2.namedWindow('phenopype' ,cv2.WINDOW_NORMAL)
+                cv2.imshow('phenopype', self.show)    
+                
             else:
                 print(self.time_stamp)
-                
-                
-            # video writer  
-            if self.capture_frame == True:    
 
-                cv2.namedWindow('phenopype' ,cv2.WINDOW_NORMAL)
-                cv2.imshow('phenopype', self.fgmask)    
-                self.writer.write(self.fgmask)
-            
             # keep stream open
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+            
         # cleanup
         self.capture.release()
         self.writer.release()
         cv2.destroyAllWindows()
         
         
-#class test_module(object):
-#    def track(**kwargs):
-#        fgmask = video.fgmask
-#        if "blur" in kwargs:
-#            blur_kernel, thresh_val = kwargs.get("blur",[9, 127])
-#            blurred = blur(fgmask, blur_kernel)
-
- 
-
-class motion_tracker(object):
-    def __init__(self, frame, fg_mask, min_length, **kwargs):
-        
-        shadows = kwargs.get("detect_shadows", True)
-
-        if shadows == True:
-            ret, fg_mask = cv2.threshold(fg_mask, 128, 255, cv2.THRESH_BINARY)
-        
-        if "blur" in kwargs:
-            blur_kernel, thresh_val = kwargs.get("blur",[9, 127])
-            self.blurred = blur(fg_mask, blur_kernel)
-            ret, self.thresh = cv2.threshold(self.blurred, thresh_val, 255, cv2.THRESH_BINARY)
+class tracking_method(object):
+    def __init__(self, **kwargs):
+        """Constructs a tracking method that can be supplied to the tracker. 
+        """                   
+        for key, value in kwargs.items():
+            if key in kwargs:
+                setattr(self, key, value)
             
+    def print_settings(self, **kwargs):
+        """Prints the settings of the tracking method. 
+        """ 
+        width = kwargs.get("width",30)
+        indent = kwargs.get("indent",1)
+        compact = kwargs.get("compact",True)
+        pretty = pprint.PrettyPrinter(width=width, compact=compact, indent=indent)
         
-        ret, contours, hierarchy = cv2.findContours(self.thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1) #CHAIN_APPROX_NONE
+        pretty.pprint(vars(self))
+        
+            
+    def _run(self, frame_in, fgmask, resize_factor, **kwargs):
+        """Run tracking method on current frame. Internal reference - don't call this directly.
+        """
+        
+        self.frame_out = frame_in
+        self.fgmask = fgmask
+        
+        if "remove_shadows" in vars(self):
+            if self.remove_shadows==True:
+                ret, self.mask = cv2.threshold(self.fgmask, 128, 255, cv2.THRESH_BINARY)
+         
+        if "blur" in vars(self):
+            blur_kernel, thresh_val = (self.blur)
+            self.fgmask = blur(self.fgmask, blur_kernel)            
+            ret, self.fgmask = cv2.threshold(self.fgmask, thresh_val, 255, cv2.THRESH_BINARY)
+                    
 
-        # fish ellipse, length, and center
+
+        # =============================================================================
+        # find objects
+        # =============================================================================
+        
+        ret, contours, hierarchy = cv2.findContours(self.fgmask, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1) #CHAIN_APPROX_NONE
+
+        # turn blobs to ellipses
         if contours:
             ellipses = []
             for cnt in contours:
@@ -175,38 +224,44 @@ class motion_tracker(object):
                     ellipses.append([0,0,0,0])
             ellipses_l = [l[3] for l in ellipses] 
             
-            # check against min length
-            if ellipses_l[np.argmax(ellipses_l)] > min_length:
-                
+        # check ellipses against min length
+        if "min_length" in vars(self):
+            if ellipses_l[np.argmax(ellipses_l)] < self.min_length:
+                self.frame_out = cv2.putText(self.frame_out, "too small", (int(self.frame_out.shape[0]/20),int(self.frame_out.shape[1]/20)), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA)                
+                return self.frame_out
+        else:
+            if len(ellipses_l) > 0:
+            
                 self.contour = contours[np.argmax(ellipses_l)]
                 self.ellipse = ellipses[np.argmax(ellipses_l)]
                 self.center = np.array([self.ellipse[0]])
     
                 # draw fish mask
-                self.mask = np.zeros_like(frame) # Create mask where white is what we want, black otherwise
+                self.mask = np.zeros_like(self.frame_out) # Create mask where white is what we want, black otherwise
                 self.contour_drawn = cv2.drawContours(self.mask, [self.contour], 0,(0,0, 255), -1) # Draw filled contour in mask     
                 
-                # make extended box  to mask for isopod detection
+                # make extended box to mask for isopod detection
                 box = cv2.minAreaRect(self.contour)
                 box = tuple([box[0], (box[1][0] + 200, box[1][1] + 150), box[2]])
                 
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.cvtColor(self.frame_out, cv2.COLOR_BGR2GRAY)
                 self.box = np.full_like(gray, 255)
                 self.box = cv2.drawContours(self.box,[np.int0(cv2.boxPoints(box))],0,0,-1)
-                               
-                self.empty = False
-                
-            else:
-                self.empty = True
-        else: 
-            self.empty = True
+    
+                self.frame_out = cv2.addWeighted(self.frame_out, 1, self.contour_drawn, 0.5, 0)
+                self.frame_out = cv2.ellipse(self.frame_out, tuple(self.ellipse[0:3]), (0,0,255), 3)
+                self.frame_out = cv2.putText(self.frame_out, self.label, (int(self.center[0,0]), int(self.center[0,1])), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA)  
+        
+                self.frame_out = cv2.resize(self.frame_out, (0,0), fx=1*resize_factor, fy=1*resize_factor) 
+
+                return self.frame_out
             
-
-        frame_out = frame
-        frame_out = cv2.addWeighted(frame_out, 1, self.contour_drawn, 0.5, 0)
-        frame_out = cv2.ellipse(frame_out, tuple(self.ellipse[0:3]), (0,0,255), 3)
-        frame_out = cv2.putText(frame_out, "Fish", (int(self.center[0,0]), int(self.center[0,1])), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA)  
-
+            else:
+                
+                self.frame_out = cv2.putText(self.frame_out, "no " + self.label + " found", (int(self.frame_out.shape[0]/10),int(self.frame_out.shape[1]/10)), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2,cv2.LINE_AA)   
+                return self.frame_out
+        
+        
 
         
 
