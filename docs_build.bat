2de822debd3734e5a5484277154014eb3f4b72ba
mluerig <moritz.luerig@gmail.com>
2020-03-18 10:05:36 +0100

    proper conf path

--- docs_build.bat
+++ docs_build.bat
@@ -1,1 +1,1 @@
-sphinx-multibuild -i tutorials -i docs_source -i examples -s docs/_temp -o docs
+sphinx-multibuild -c E:\git_repos\phenopype -i tutorials -i docs_source -i examples -s docs/_temp -o docs
