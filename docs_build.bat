8ab0efd710475541f96c01e5fd803166465828a7
mluerig <moritz.luerig@gmail.com>
2020-03-25 15:17:26 +0100

    fix doc dirs

--- docs_build.bat
+++ docs_build.bat
@@ -1,7 +1,1 @@
-move docs\_static _temp\_static
-rmdir E:\git_repos\phenopype\_temp\docs /S /Q
-rmdir E:\git_repos\phenopype\docs /S /Q
-mkdir E:\git_repos\phenopype\_temp\docs
-mkdir E:\git_repos\phenopype\docs
-move _temp\_static docs\_static 
 sphinx-multibuild -c E:\git_repos\phenopype -i tutorials -i _source -s _temp/docs -o docs
