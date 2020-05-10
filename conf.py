1835890cad51b9a4c648391d99bb0a1aeb1fac46
mluerig <moritz.luerig@gmail.com>
2020-05-10 16:10:02 +0200

    gi fix/tut

--- conf.py
+++ conf.py
@@ -85,6 +85,7 @@ pygments_style = 'sphinx'
 #
 html_theme = 'bootstrap'
 html_show_sourcelink = True
+html_last_updated_fmt = '%b %d, %Y, %X'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
