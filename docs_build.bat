087ab53e6c9358c6c95b41910cd6d85ee8bdddf0
mluerig <moritz.luerig@gmail.com>
2020-05-11 12:01:50 +0200

    restore gi

---
+++ docs_build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+sphinx-multibuild -c . -i source -i tutorials -s _temp/docs -o docs -a 
+
+if "%~1"=="-o" (
+	cd docs 
+	rmdir ".git" /S /Q
+	git init 
+	@echo .doctrees > docs/.gitignore	
+	git add . 
+	git commit -m "- no version control in docs -" 
+	git remote add origin git@github.com:mluerig/phenopype
+	git push --force origin master:gh-pages 
+	cd ..
+) 
