5e371aae9af5c90089b8357747f2ea3eae5fcbaa
mluerig <moritz.luerig@gmail.com>
2020-06-04 16:32:07 +0200

    new dist for fix

--- dist_fix.bat
+++ dist_fix.bat
@@ -1,13 +1,13 @@
 RMDIR "dist/fix" /S /Q
-python setup.py sdist --format=zip
-dir /s /b dist\*.zip
-7z x E:\git_repos\phenopype\dist\phenopype-1.0.5.zip -odist/fix
-	cd dist/fixt 
-	git init 
-	git add . 
-	git commit -m "- no version control in hotfixes -" 
-	git remote add origin git@github.com:mluerig/phenopype
-	git push --force origin master:fix
-	cd ..
-) 
-
+python setup.py sdist
+robocopy phenopype dist/fix/phenopype
+robocopy phenopype.egg-info dist/fix/phenopype.egg-info
+robocopy "E:/git_repos/phenopype" dist/fix MANIFEST.in README.md setup.py
+cd dist/fix 
+git init 
+git add . 
+git commit -m "- no version control in hotfix -" 
+git remote add origin git@github.com:mluerig/phenopype
+git push --force origin master:fix
+cd ..
+cd ..
