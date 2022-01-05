cc01b58609e577c87fb0dd225e6962e4607dec97
mluerig <moritz.luerig@gmail.com>
2022-01-05 09:55:11 +0100

    fix test prep

--- tests/test_00_prep.py
+++ tests/test_00_prep.py
@@ -14,7 +14,7 @@ def refresh_test_dir(settings):
     if os.path.isdir(pytest.test_dir):
        shutil.rmtree(pytest.test_dir) 
        print("Removed existing test dir {}".format(os.path.abspath(pytest.test_dir)))
-    os.mkdir(pytest.test_dir)
+    os.makedirs(pytest.test_dir)
 
 def test_prep(refresh_test_dir):
     pass
