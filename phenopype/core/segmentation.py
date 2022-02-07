fix decompose / convert ann / seg masking

phenopype/core/segmentation.py
@@ -411,6 +411,7 @@ def threshold(
     blocksize=99,
     value=127,
     channel=None,
+    mask=True,
     invert=False,
     **kwargs,
 ):
@@ -513,41 +514,41 @@ def threshold(
 
     # =============================================================================
     # execute masking
-
-    if "data" in annotation_mask:
-        if settings._mask_type in annotation_mask["data"]:
-            mask_bool, include_idx, exclude_idx = (
-                np.zeros(thresh.shape, dtype=bool),
-                0,
-                0,
-            )
-
-            polygons = annotation_mask["data"][settings._mask_type]
-            include = annotation_mask["data"]["include"]
-
-            if include == True:
-                for coords in polygons:
-                    mask_bool = np.logical_or(
-                        mask_bool, utils_lowlevel._create_mask_bool(thresh, coords)
-                    )
-                    include_idx += 1
-                thresh[mask_bool == False] = 0
-            elif include == False:
+    if mask:
+        if "data" in annotation_mask:
+            if settings._mask_type in annotation_mask["data"]:
+                mask_bool, include_idx, exclude_idx = (
+                    np.zeros(thresh.shape, dtype=bool),
+                    0,
+                    0,
+                )
+    
+                polygons = annotation_mask["data"][settings._mask_type]
+                include = annotation_mask["data"]["include"]
+    
+                if include == True:
+                    for coords in polygons:
+                        mask_bool = np.logical_or(
+                            mask_bool, utils_lowlevel._create_mask_bool(thresh, coords)
+                        )
+                        include_idx += 1
+                    thresh[mask_bool == False] = 0
+                elif include == False:
+                    for coords in polygons:
+                        thresh[utils_lowlevel._create_mask_bool(thresh, coords)] = 0
+                        exclude_idx += 1
+    
+                if exclude_idx > 0 and verbose:
+                    print("- excluding pixels from " + str(exclude_idx) + " drawn masks ")
+                if include_idx > 0 and verbose:
+                    print("- including pixels from " + str(include_idx) + " drawn masks ")
+    
+        if "data" in annotation_ref:
+            if settings._mask_type in annotation_ref["data"]:
+                polygons = annotation_ref["data"][settings._mask_type]
                 for coords in polygons:
                     thresh[utils_lowlevel._create_mask_bool(thresh, coords)] = 0
-                    exclude_idx += 1
-
-            if exclude_idx > 0 and verbose:
-                print("- excluding pixels from " + str(exclude_idx) + " drawn masks ")
-            if include_idx > 0 and verbose:
-                print("- including pixels from " + str(include_idx) + " drawn masks ")
-
-    if "data" in annotation_ref:
-        if settings._mask_type in annotation_ref["data"]:
-            polygons = annotation_ref["data"][settings._mask_type]
-            for coords in polygons:
-                thresh[utils_lowlevel._create_mask_bool(thresh, coords)] = 0
-            print("- excluding pixels from reference")
+                print("- excluding pixels from reference")
 
     # =============================================================================
     # return

phenopype/utils_lowlevel.py
@@ -1327,7 +1327,7 @@ def _overwrite_check_file(path, overwrite):
 #%% functions - VARIOUS
 
 
-def _convert_arr_tup_list(arr_list):
+def _convert_arr_tup_list(arr_list, add_first=False):
 
     if not arr_list.__class__.__name__ == "list":
         arr_list = [arr_list]
@@ -1335,8 +1335,20 @@ def _convert_arr_tup_list(arr_list):
     tup_list = []
     for array in arr_list:
         point_list = []
-        for point in array:
-            point_list.append(tuple((int(point[0][0]), int(point[0][1]))))
+        
+        for idx, point in enumerate(array):
+            if type(array) == np.ndarray:
+                point_converted = tuple((int(point[0][0]), int(point[0][1])))
+            elif type(array) == list:
+                point_converted = tuple((int(point[0]), int(point[1])))
+            point_list.append(point_converted)
+            if idx == 0:
+                first_point = point_converted
+                
+        ## add first point during contour->mask conversion
+        if add_first:
+            point_list.append(first_point)
+            
         tup_list.append(point_list)
 
     return tup_list