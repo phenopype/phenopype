89db4c19cda535244987df72eb9745d6f13e9370
mluerig <moritz.luerig@gmail.com>
2022-09-16 09:29:26 +0200

    fix furo theme / slim install instructions

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'phenopype'
-copyright = '2022,'
+project_copyright = '2022, Moritz Lürig'
 author = 'Moritz Lürig'
 
 from phenopype._version import __version__ as version
